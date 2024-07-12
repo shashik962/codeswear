3d24f12b68fca887ae18f2926912b264d74a337d
shashi_kumar <shashi.kumar@gingerwebs.co.in>
2024-07-12 16:40:57 +0530

    project setup or create pages for codeswear

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+          <title>Create next app</title>
+      </Head>
+        This is codeswear
+         
+    </div>
+  );
+}
---
+++ pages/tshirt.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Tshirt() {
+  return (
+    <div>
+      Tshirt
+    </div>
+  )
+}
+
+export default Tshirt
